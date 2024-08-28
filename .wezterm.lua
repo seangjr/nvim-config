838073c22e818306e26fc87007f6374a30c0c578
seangjr <sean@relampagos.org>
2024-08-28 15:19:49 +0800

    refactor: updated wezterm bg color

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,8 +8,13 @@ local config = wezterm.config_builder()
 
 -- For example, changing the color scheme:
 config.color_scheme = 'Tokyo Night'
+config.colors = {
+  background = '#00141A',
+}
 config.font = wezterm.font '0xProto Nerd Font Mono'
 config.default_prog = {'powershell.exe'}
+
+
 
 -- and finally, return the configuration to wezterm
 return config
